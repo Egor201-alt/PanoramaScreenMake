e4fc71b35bddd865567e756ad1f4c8ff9c992dbe
Egor201 <Egorsimikin934@gmail.com>
2026-02-23 23:28:47 +0300

    Add support for modifying width and height with 4 args

--- src/main/java/me/egor201/panorama_screenmake/mixin/SimpleFramebufferMixin.java
+++ src/main/java/me/egor201/panorama_screenmake/mixin/SimpleFramebufferMixin.java
@@ -9,9 +9,9 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(SimpleFramebuffer.class)
 public class SimpleFramebufferMixin {
-
+    
     @ModifyVariable(method = "<init>(IIZ)V", at = @At("HEAD"), ordinal = 0, argsOnly = true, require = 0)
-    private int modifyWidth(int width) {
+    private int modifyWidth3Args(int width) {
         if (PanoramaCraft.isCapturingPanorama && ModConfig.INSTANCE.resolution > 0) {
             return ModConfig.INSTANCE.resolution;
         }
@@ -19,7 +19,23 @@ public class SimpleFramebufferMixin {
     }
 
     @ModifyVariable(method = "<init>(IIZ)V", at = @At("HEAD"), ordinal = 1, argsOnly = true, require = 0)
-    private int modifyHeight(int height) {
+    private int modifyHeight3Args(int height) {
+        if (PanoramaCraft.isCapturingPanorama && ModConfig.INSTANCE.resolution > 0) {
+            return ModConfig.INSTANCE.resolution;
+        }
+        return height;
+    }
+
+    @ModifyVariable(method = "<init>(IIZZ)V", at = @At("HEAD"), ordinal = 0, argsOnly = true, require = 0)
+    private int modifyWidth4Args(int width) {
+        if (PanoramaCraft.isCapturingPanorama && ModConfig.INSTANCE.resolution > 0) {
+            return ModConfig.INSTANCE.resolution;
+        }
+        return width;
+    }
+
+    @ModifyVariable(method = "<init>(IIZZ)V", at = @At("HEAD"), ordinal = 1, argsOnly = true, require = 0)
+    private int modifyHeight4Args(int height) {
         if (PanoramaCraft.isCapturingPanorama && ModConfig.INSTANCE.resolution > 0) {
             return ModConfig.INSTANCE.resolution;
         }
