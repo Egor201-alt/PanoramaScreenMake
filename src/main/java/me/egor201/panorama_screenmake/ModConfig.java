a8468f57da5d6bc20725960e8cfa7ae670a89597
Egor201 <Egorsimikin934@gmail.com>
2026-02-23 20:28:34 +0300

    Refactor ModConfig to use Gson for JSON config

--- src/main/java/me/egor201/panorama_screenmake/ModConfig.java
+++ src/main/java/me/egor201/panorama_screenmake/ModConfig.java
@@ -1,12 +1,46 @@
 package me.egor201.panorama_screenmake;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ModConfig {
 
-    public static final ModConfig INSTANCE = new ModConfig();
+    public static ModConfig INSTANCE = new ModConfig();
+    
+    private static final File CONFIG_FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), "panoramascreenmake.json");
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     public int resolution = 1024;
-
     public String savePath = "";
-
     public int delaySeconds = 0;
+
+    public static void load() {
+        if (CONFIG_FILE.exists()) {
+            try (FileReader reader = new FileReader(CONFIG_FILE)) {
+                INSTANCE = GSON.fromJson(reader, ModConfig.class);
+                
+                if (INSTANCE.delaySeconds < 0) INSTANCE.delaySeconds = 0;
+                if (INSTANCE.delaySeconds > 10) INSTANCE.delaySeconds = 10;
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            save();
+        }
+    }
+
+    public static void save() {
+        try (FileWriter writer = new FileWriter(CONFIG_FILE)) {
+            GSON.toJson(INSTANCE, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
