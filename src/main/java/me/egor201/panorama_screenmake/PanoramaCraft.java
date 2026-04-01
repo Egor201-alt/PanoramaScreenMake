ac903c8fd6a27ba875845dffc2fbf2916448bdb1
Egor201 <Egorsimikin934@gmail.com>
2026-04-02 02:21:47 +0300

    Update imports in PanoramaCraft.java

--- src/main/java/me/egor201/panorama_screenmake/PanoramaCraft.java
+++ src/main/java/me/egor201/panorama_screenmake/PanoramaCraft.java
@@ -4,9 +4,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.client.KeyMapping
+import net.minecraft.client.InputConstants;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
