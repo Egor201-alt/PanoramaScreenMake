feff9a0006d95749feb3c0650520ca36175f3c95
Egor201 <Egorsimikin934@gmail.com>
2026-06-19 11:38:35 +0300

    Fix GlCommandEncoderMixin: use CallbackInfoReturnable for awaitSubmit

--- src/client/java/me/egor201/panorama_screenmake/mixin/GlCommandEncoderMixin.java
+++ src/client/java/me/egor201/panorama_screenmake/mixin/GlCommandEncoderMixin.java
@@ -5,7 +5,7 @@ import org.lwjgl.opengl.GL11C;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // MC 26.2 forbids waiting on a fence while a command encoder submit is active.
 // Sodium's MappableRingBuffer hits this restriction during panorama capture.
@@ -15,10 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class GlCommandEncoderMixin {
 
     @Inject(method = "awaitSubmit", at = @At("HEAD"), cancellable = true, remap = false)
-    private void panoramaScreenmake$safeAwaitSubmit(CallbackInfo ci) {
+    private void panoramaScreenmake$safeAwaitSubmit(CallbackInfoReturnable<?> cir) {
         if (PanoramaCraft.panoramaCapturing) {
             GL11C.glFinish();
-            ci.cancel();
+            cir.setReturnValue(null);
         }
     }
 }
